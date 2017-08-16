208c3268f96d4d1463bde1faed79288d6bd7add4
Josh Holder <josh.holder@theguardian.com>
2017-08-16 15:51:04 +0100

    v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,44 @@
-import GoogleMap from '../components/interactive-google-map/index.html'
-import AutoplayingVideo from '../components/interactive-autoplaying-video/index.html'
+let scrollPos = 0;
 
-const el = document.querySelector(".here")
+const pTags = [].slice.apply(document.querySelectorAll(".interactive-seabed__text"));
 
-// const map = new GoogleMap({
-//     target: el,
-//     data: {
-//         el: el,
-//         config: {
-//             center: { lat: 30.7316306, lng: -94.9396368 },
-//             zoom: 3
-//         },
-//         markers: [{
-//             "lat": 47.2655779,
-//             "lng": -68.59197619999999,
-//             "label": "A label"
-//         }],
-//         markerConfig: {
-//             markerSize: 20,
-//             infoWindowWidth: 100
-//         },
-//         key: "AIzaSyBGZVyAXHJwoA4Ea-a3kuD1AsuZwbrnLlM"
-//     }
-// })
+const videoEl = document.querySelector(".interactive-seabed__video video");
 
-const vid = new AutoplayingVideo({
-    target: el,
-    data: {
-        el: el,
-        poster: "http://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_768k_H264_poster.jpg",
-        sources: {
-            "small": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_768k_H264.mp4",
-            "medium": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_2M_H264.mp4",
-            "large": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_4M_H264.mp4"
+const timings = [3, 14, 20, 40, 44];
+
+const checkTags = () => {
+    if (window.pageYOffset !== scrollPos) {
+        scrollPos = window.pageYOffset;
+        let passedPs = [];
+
+        pTags.forEach((p, i) => {
+            const bbox = p.getBoundingClientRect();
+
+            if(bbox.top < 0) {
+                passedPs.push(p);
+            }
+        });
+
+        const timingToAimFor = timings[passedPs.length - 1];
+
+        if(timingToAimFor > videoEl.currentTime) {
+            videoEl.play();
+
+            shouldPauseVideo(timingToAimFor);
         }
-    }
-})
 
+    }
+    requestAnimationFrame(checkTags);
+}
+
+const shouldPauseVideo = (timingToAimFor) => {
+    if(timingToAimFor > videoEl.currentTime) {
+        requestAnimationFrame(() => {
+            shouldPauseVideo(timingToAimFor);
+        });
+    } else {
+        videoEl.pause();
+    }
+}
+
+checkTags()
--- src/render.js
+++ src/render.js
@@ -1,11 +1,5 @@
-const requireUncached = require('require-uncached');
-
 import mainTemplate from './src/templates/main.html!text'
-import rp from "request-promise"
-import 'svelte/ssr/register'
-
-const Table = requireUncached('../src/components/interactive-table/render.html')
 
 export async function render() {
-    return `<div class="here"></div>`;
+    return mainTemplate;
 }
