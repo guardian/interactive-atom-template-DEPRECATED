e8f7392c1c4095d1db1d1b95770d11cd4cc7f2d6
Josh Holder <josh.holder@theguardian.com>
2017-08-16 13:46:00 +0100

    v1 autoplay

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,39 @@
 import GoogleMap from '../components/interactive-google-map/index.html'
+import AutoplayingVideo from '../components/interactive-autoplaying-video/index.html'
 
 const el = document.querySelector(".here")
 
-console.log(el);
+// const map = new GoogleMap({
+//     target: el,
+//     data: {
+//         el: el,
+//         config: {
+//             center: { lat: 30.7316306, lng: -94.9396368 },
+//             zoom: 3
+//         },
+//         markers: [{
+//             "lat": 47.2655779,
+//             "lng": -68.59197619999999,
+//             "label": "A label"
+//         }],
+//         markerConfig: {
+//             markerSize: 20,
+//             infoWindowWidth: 100
+//         },
+//         key: "AIzaSyBGZVyAXHJwoA4Ea-a3kuD1AsuZwbrnLlM"
+//     }
+// })
 
-const map = new GoogleMap({
+const vid = new AutoplayingVideo({
     target: el,
     data: {
         el: el,
-        config: {
-        	center: { lat: 30.7316306, lng: -94.9396368 },
-        	zoom: 3
-        },
-        markers: [{
-            "lat": 47.2655779,
-            "lng": -68.59197619999999,
-            "label": "A label"
-        }],
-        markerConfig: {
-        	markerSize: 20,
-        	infoWindowWidth: 100
-        },
-        key: "AIzaSyBGZVyAXHJwoA4Ea-a3kuD1AsuZwbrnLlM"
+        poster: "http://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_768k_H264_poster.jpg",
+        sources: {
+            "small": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_768k_H264.mp4",
+            "medium": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_2M_H264.mp4",
+            "large": "https://cdn.theguardian.tv/interactive/2015/08/25/150825TwickenhamCarPark_4M_H264.mp4"
+        }
     }
 })
+
