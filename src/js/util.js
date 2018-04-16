f6de3f7fac92fc4f2e89b6be8f6c0eba9bcedcca
Niko Kommenda <nkommenda@hotmail.com>
2018-04-16 16:17:16 +0100

    add some common js utilities

---
+++ src/js/util.js
@@ -0,0 +1,33 @@
+const $ = selector => document.querySelector(selector)
+const $$ = selector => [].slice.apply(document.querySelectorAll(selector))
+
+const round = (value, exp) => {
+	if (typeof exp === 'undefined' || +exp === 0)
+		return Math.round(value);
+
+	value = +value;
+	exp = +exp;
+
+	if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+		return NaN;
+
+	value = value.toString().split('e');
+	value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+	value = value.toString().split('e');
+	return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+}
+
+const numberWithCommas = x => {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+const wait = ms => new Promise((resolve, reject) => setTimeout(() => resolve(), ms ))
+
+const getDimensions = el => {
+	const width = svgEl.clientWidth || svgEl.getBoundingClientRect().width
+	const height = svgEl.clientHeight || svgEl.getBoundingClientRect().height
+	return [ width, height ]
+}
+
+export { $, $$, round, numberWithCommas, wait, getDimensions }
