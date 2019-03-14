5cf7d0960b556a53b162566081d6f5a399db14e3
Niko Kommenda <nkommenda@hotmail.com>
2019-03-14 11:43:26 +0000

    fix SASS palette, add new utility functions

--- src/js/util.js
+++ src/js/util.js
@@ -1,6 +1,8 @@
 const $ = selector => document.querySelector(selector)
 const $$ = selector => [].slice.apply(document.querySelectorAll(selector))
 
+// rounds value to exp digits (can be a negative number -> eg round(1234, -3) === 1000)
+
 const round = (value, exp) => {
 	if (typeof exp === 'undefined' || +exp === 0)
 		return Math.round(value);
@@ -18,9 +20,13 @@ const round = (value, exp) => {
 	return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
 }
 
+// inserts UK-style commas in numbers (1,300,000)
+
 const numberWithCommas = x => {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
+
+// returns a Promise that resolves after ms milliseconds
 
 const wait = ms => new Promise((resolve, reject) => setTimeout(() => resolve(), ms ))
 
@@ -41,6 +47,31 @@ const hashPattern = (patternId, pathClass, rectClass, a = 4) => {
 
 }
 
+function ordinal(i) {
+
+	if(i === 1) { return 'first' }
+	if(i === 2) { return 'second' }
+	if(i === 3) { return 'third' }
+
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+const sum = (a, b) => a + b
+
+// duplicates a DOM element, inserts BEFORE original and adds a class name without deleting the old one.
+// Great for adding white outlines to SVG text elements
+
 const duplicate = ( el, className ) => {
 
 	const clone = el.cloneNode(true)
@@ -49,14 +80,16 @@ const duplicate = ( el, className ) => {
 
 }
 
+// sequentially executes the (Promise-based) tasks in arr
+
 const pseq = (arr, lambda) => {
 
-	return arr.reduce( (agg, cur) => {
+	return arr.reduce( (agg, cur, i, arr) => {
 
-		return agg.then(res => lambda(cur).then( res2 => res.concat(res2)))
+		return agg.then(res => lambda(cur, i, arr).then( res2 => res.concat(res2)))
 
 	}, Promise.resolve([]) )
 
 }
 
-export { $, $$, round, numberWithCommas, wait, getDimensions, hashPattern, duplicate, pseq }
+export { $, $$, round, numberWithCommas, wait, getDimensions, hashPattern, duplicate, pseq, sum, ordinal }
