7d5ce75a6559e41ea9b3e68c82ccc929781005d4
superfrank <frank.hulley-jones@guardian.co.uk>
2019-07-30 12:12:38 +0100

    moon-exercise

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-// js code here
+const wrapperEl = document.querySelector('.gv-timeline');
+
+window.addEventListener('scroll', function(e) {
+  const wrapperBBox = wrapperEl.getBoundingClientRect();
+  const percentageScrolled = (wrapperBBox.top > 0) ? 0 : Math.abs(wrapperBBox.top) / wrapperBBox.height;
+
+  if (percentageScrolled >= 0.5) {
+    document.querySelector('.gv-photoblock__two').style.display="block";
+    document.querySelector('.gv-photoblock__one').style.display="none";
+  } else {
+    document.querySelector('.gv-photoblock__two').style.display="none";
+    document.querySelector('.gv-photoblock__one').style.display="block";
+  }
+});
