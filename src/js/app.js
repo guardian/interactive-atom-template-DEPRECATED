1fd12e50b6634a0ed4054d80854cf31c598687e6
Zef Cherry-Kynaston <zef_cherry-kynaston@35054.gnm.int>
2019-09-23 13:56:58 +0100

    Hope is power

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-// js code here
+function checkLocation(){
+    if (localStorage && localStorage.getItem('gu.geolocation')) {
+      if (["GB"].includes(JSON.parse(localStorage.getItem('gu.geolocation') ).value)) {
+        return 'uk';
+      } else if(["US"].includes(JSON.parse(localStorage.getItem('gu.geolocation') ).value)) {
+        return 'us';
+      } else if (["AU"].includes(JSON.parse(localStorage.getItem('gu.geolocation') ).value)) {
+        return 'aus'
+      } else {
+        return 'uk'
+      }
+    }
+  }
+checkLocation();  
+  
+  if(checkLocation() === 'uk'){
+    document.querySelector('.regional').classList.add('uk');
+  } else if(checkLocation() === 'us') {
+    document.querySelector('.regional').classList.add('us');
+  } else if (checkLocation() === 'aus') {
+    document.querySelector('.regional').classList.add('aus');
+  }
