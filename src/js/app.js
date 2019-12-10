d0b55b37249ff492f227f20928c8bd990095631e
blongden73 <hello@benlongdendesign.co.uk>
2019-12-10 17:16:07 +0000

    leader temp

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 // js code here
+
+var headline = document.querySelector('.content__headline');
+var split = headline.innerHTML.split(':');
+headline.innerHTML = '<span class="kicker">'+ split[0] +'</span>' + '<span class="headline">'+ split[1] +'</span>';
+console.log(headline);
+console.log(split);
