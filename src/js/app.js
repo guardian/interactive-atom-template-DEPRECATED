a8d064c469f9e846645bbf14ee1dd5466152b714
blongden73 <hello@benlongdendesign.co.uk>
2020-01-31 11:25:21 +0000

    eu-timeline

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,86 @@
-// js code here
+var wrapper = document.querySelector('.bj_wrapper');
+var borisClicker = document.querySelector('.bj_progress_boris');
+var arrow = document.querySelector('.bj_content_arrow');
+var contentStart = document.querySelector('#content-start');
+var count = 0;
+
+
+var options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+}
+
+var observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate');
+    }
+  });
+}, options);
+
+//scrolling animations
+var els = document.querySelectorAll('.tug, .garden, .bike, .rugby, .annoyed');
+els.forEach(el => observer.observe(el));
+
+
+/* We define our function 🕹 */
+function replaceVerticalScrollByHorizontal(event) {
+  if (event.deltaY != 0) {
+    // manually scroll horizonally instead
+    window.scroll(window.scrollX + event.deltaY * 5, window.scrollY);
+
+    // prevent vertical scroll
+    event.preventDefault();
+  }
+  return;
+}
+
+window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
+
+
+window.addEventListener('scroll', function(){
+  var width = document.querySelector('.bj_content_flex');
+  var boris = document.querySelector('.bj_progress_boris');
+  var scrollPercentage = 100 * this.scrollX / (width.offsetWidth - window.innerWidth);
+  boris.setAttribute('style', 'left:'+scrollPercentage+'%');
+  if(scrollPercentage >= 1) {
+    boris.classList.add('active');
+  } else {
+    boris.classList.remove('active');
+  }
+})
+
+borisClicker.addEventListener('click', function(){
+  console.log(count);
+  this.classList.add('shake');
+  count ++;
+  if(count === 6) {
+    this.classList.add('fall');
+    setTimeout(function(){
+      borisClicker.classList.remove('fall');
+      count = 0;
+    }, 5000);
+    setTimeout(function(){
+      borisClicker.classList.add('putback');
+    }, 5000);
+    setTimeout(function(){
+      borisClicker.classList.remove('putback');
+    }, 6500);
+  }
+  setTimeout(function(){
+    borisClicker.classList.remove('shake');
+  }, 2000);
+});
+
+arrow.addEventListener('click', function(){
+  var position = contentStart.getBoundingClientRect();
+  console.log(position);
+  window.scroll({
+  top: 0,
+  left: position.left - 20,
+  behavior: 'smooth'
+});
+});
--- src/render.js
+++ src/render.js
@@ -1,8 +1,14 @@
-import templateHTML from "./src/templates/main.html!text"
+import mainTemplate from './src/templates/main.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    // this function just has to return a string of HTML
-    // you can generate this using js, e.g. using Mustache.js
-
-    return templateHTML;
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1GlbtduHKO486SoYd7QnX__F4Fph16mIP2q9sHet-w8A.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
 }
