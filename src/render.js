7b87f9c48c925e25e0ef8f02379547535434371b
blongden73 <hello@benlongdendesign.co.uk>
2020-02-12 15:45:57 +0000

    added besn stuff

--- src/render.js
+++ src/render.js
@@ -1,8 +1,14 @@
-import templateHTML from "./src/templates/main.html!text"
+import mainTemplate from './src/templates/main.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    // this function just has to return a string of HTML
-    // you can generate this using js, e.g. using Mustache.js
-
-    return templateHTML;
+export function render() {
+    return rp({
+        uri: 'https://content.guardianapis.com/us-news/live/2020/feb/06/donald-trump-speech-impeachment-verdict-romney-buttigieg-sanders-biden-live-latest-news?format=json&api-key=teleporter-view&show-elements=all&show-atoms=all&show-rights=all&show-fields=all&show-tags=all&show-blocks=all&show-references=all',
+        json: true
+    }).then((data) => {
+        var newData = data.response.content;
+        var html = Mustache.render(mainTemplate, newData);
+        return html;
+    });
 }
