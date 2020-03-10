1e0bd680dbed45effad60f14112de23186046200
Harry Fischer <harry.fischer@guardian.co.uk>
2020-03-10 13:28:11 +0000

    firstcommit

--- src/render.js
+++ src/render.js
@@ -4,5 +4,14 @@ export async function render() {
     // this function just has to return a string of HTML
     // you can generate this using js, e.g. using Mustache.js
 
-    return templateHTML;
+    return '<div id="gd-timeline" style="background-color:white;">&nbsp;</div>';
 }
+
+
+
+// export async function render() {
+//     // this function just has to return a string of HTML
+//     // you can generate this using js, e.g. using Mustache.js
+
+//     return templateHTML;
+// }
