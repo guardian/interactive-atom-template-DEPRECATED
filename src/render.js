0e6f5e1ec4eca8cca5b8c5b7bb87f9caa9cfb2d9
Seán mór Clarke <sean.clarke@guardian.co.uk>
2020-03-10 15:48:43 +0000

    Revert "firstcommit"
    This reverts commit 1e0bd680dbed45effad60f14112de23186046200.

--- src/render.js
+++ src/render.js
@@ -4,14 +4,5 @@ export async function render() {
     // this function just has to return a string of HTML
     // you can generate this using js, e.g. using Mustache.js
 
-    return '<div id="gd-timeline" style="background-color:white;">&nbsp;</div>';
+    return templateHTML;
 }
-
-
-
-// export async function render() {
-//     // this function just has to return a string of HTML
-//     // you can generate this using js, e.g. using Mustache.js
-
-//     return templateHTML;
-// }
