d77a2481043db216379a97724a85e097849310fb
Harry Fischer <harry.fischer@guardian.co.uk>
2020-04-22 13:45:47 +0100

    adding healthworkers interactive - CarolineBannock

--- src/render.js
+++ src/render.js
@@ -1,8 +1,24 @@
-import templateHTML from "./src/templates/main.html!text"
+// import templateHTML from "./src/templates/main.html!text"
 
-export async function render() {
-    // this function just has to return a string of HTML
-    // you can generate this using js, e.g. using Mustache.js
+// export async function render() {
+//     // this function just has to return a string of HTML
+//     // you can generate this using js, e.g. using Mustache.js
 
-    return templateHTML;
+//     return templateHTML;
+// }
+
+import mainTemplate from './src/templates/main.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1KWFQ1_AYZud761yo9auWYlwdZB7VvACRUmZ0UbbAQ2M.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
 }
